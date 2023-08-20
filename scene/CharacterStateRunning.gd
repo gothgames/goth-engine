2dbc001f9da5aa8f94864e052c50587d957174c9
goth-king <122706209+goth-king@users.noreply.github.com>
2023-08-20 09:58:12 -0700

    run animation while running

--- scene/CharacterStateRunning.gd
+++ scene/CharacterStateRunning.gd
@@ -7,6 +7,7 @@ extends CharacterState
 
 func enter_state():
 	print("Running")
+	sm.animation_tree.set("parameters/RunBlend/blend_amount",1.0)
 	pass # Replace with function body.
 
 
@@ -17,7 +18,7 @@ func physics_step(delta):
 	elif sm.input.move_force == 0:
 		sm.change_state(standing_state)
 	else:
-		sm.body.velocity = sm.input.move_vector * speed * delta
+		sm.body.velocity = sm.input.move_vector * speed
 		sm.body.move_and_slide()
 		sm.body.look_at(sm.body.position + sm.body.velocity)
 	pass
--- scene/CharacterStateStanding.gd
+++ scene/CharacterStateStanding.gd
@@ -5,11 +5,13 @@ extends CharacterState
 
 func enter_state():
 	print("Standing")
+	sm.animation_tree.set("parameters/RunBlend/blend_amount",0.0)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func physics_step(_delta):
+	
 	
 	if not sm.body.is_on_floor():
 		sm.change_state(falling_state)
